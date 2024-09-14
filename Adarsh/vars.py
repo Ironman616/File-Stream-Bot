316c9d9facfec146585cd0ca1876c931bd50d64e
Ironman616 <150930956+Ironman616@users.noreply.github.com>
2024-09-14 15:26:20 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -32,9 +32,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL', True))
     if HAS_SSL:
-        URL = "bots.hostingup.icu:9005".format(FQDN)
+        URL = "bots.hostingup.icu:9005/".format(FQDN)
     else:
-        URL = "bots.hostingup.icu:9005".format(FQDN)
+        URL = "bots.hostingup.icu:9005/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://primebaby11220:TuGa0I5ZsiC8mp5A@cluster0.df4rivj.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Movie_loverzz'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))
